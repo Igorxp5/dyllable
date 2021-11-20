b608c35806982f5930263991fb63a63abbbcc087
Igorxp5 <rogixp5@gmail.com>
2021-11-20 15:10:18 -0300

    Create ActionPacket and Parse DiscoveryPacket
    - Create tests for parse DiscoveryPacket
    - Create Request and Response ActionPacket. TODO: Create tests
    for creating them; Parse them.

--- network/packet.go
+++ network/packet.go
@@ -1,13 +1,21 @@
 package network
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net"
+	"strconv"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 const preffix = "DYLLABLE-"
-const discoveryPreffix = preffix + "DISCOVERY"
+const actionPreffix = preffix + "ACTION-"
+const discoveryIdentifier = preffix + "DISCOVERY"
 
 const headerSeparator = "\r\n"
 const endPacket = "\r\n"
@@ -15,21 +23,31 @@ const endPacket = "\r\n"
 const requestDiscoveryType = "DISCOVERY"
 const responseDiscoveryType = "RUNNING-APP"
 
+const requestActionIdentifier = actionPreffix + "REQUEST"
+const responseActionIdentifier = actionPreffix + "RESPONSE"
+
+type Packet interface {
+	String() (string, error)
+	Bytes() ([]byte, error)
+}
+
 type DiscoveryPacket struct {
 	Address net.IP
 	Port    uint16
 	Type    string
 }
 
-func (packet *DiscoveryPacket) String() string {
+func (packet *DiscoveryPacket) String() (string, error) {
 	typeHeader := fmt.Sprintf("TYPE: %s", packet.Type)
 	hostHeader := fmt.Sprintf("HOST: %s:%d", packet.Address, packet.Port)
-	headers := []string{discoveryPreffix, typeHeader, hostHeader}
-	return strings.Join(headers, headerSeparator) + headerSeparator + endPacket
+	headers := []string{discoveryIdentifier, typeHeader, hostHeader}
+	return strings.Join(headers, headerSeparator) + headerSeparator + endPacket, nil
 }
 
-func (packet *DiscoveryPacket) Bytes() []byte {
-	return []byte(packet.String())
+func (packet *DiscoveryPacket) Bytes() (out []byte, err error) {
+	packetString, err := packet.String()
+	out = []byte(packetString)
+	return
 }
 
 func NewRequestDiscoveryPacket(address net.IP, port uint16) DiscoveryPacket {
@@ -38,4 +56,163 @@ func NewRequestDiscoveryPacket(address net.IP, port uint16) DiscoveryPacket {
 
 func NewResponseDiscoveryPacket(address net.IP, port uint16) DiscoveryPacket {
 	return DiscoveryPacket{address, port, responseDiscoveryType}
+}
+
+type RequestActionPacket struct {
+	RequestUUID uuid.UUID
+	ActionId    uint8
+	Parameters  map[string]interface{}
+}
+
+type ResponseActionPacket struct {
+	RequestUUID uuid.UUID
+	Approved    bool
+	Content     map[string]interface{}
+}
+
+func (packet *RequestActionPacket) String() (out string, err error) {
+	requestUUIDHeader := fmt.Sprintf("REQUEST-UUID: %s", packet.RequestUUID.String())
+	actionHeader := fmt.Sprintf("ACTION-ID: %d", packet.ActionId)
+	parametersJSON, err := json.Marshal(packet.Parameters)
+	if err != nil {
+		return
+	}
+	headers := []string{requestActionIdentifier, requestUUIDHeader, actionHeader, string(parametersJSON)}
+	out = strings.Join(headers, headerSeparator) + headerSeparator + endPacket
+	return
+}
+
+func (packet *RequestActionPacket) Bytes() (out []byte, err error) {
+	packetString, err := packet.String()
+	out = []byte(packetString)
+	return
+}
+
+func (packet *ResponseActionPacket) String() (out string, err error) {
+	requestUUIDHeader := fmt.Sprintf("REQUEST-UUID: %s", packet.RequestUUID.String())
+	var approved string
+	if packet.Approved {
+		approved = "True"
+	} else {
+		approved = "False"
+	}
+	approvedHeader := fmt.Sprintf("APPROVED: %s", approved)
+	parametersJSON, err := json.Marshal(packet.Content)
+	if err != nil {
+		return
+	}
+	headers := []string{responseActionIdentifier, requestUUIDHeader, approvedHeader, string(parametersJSON)}
+	out = strings.Join(headers, headerSeparator) + headerSeparator + endPacket
+	return
+}
+
+func (packet *ResponseActionPacket) Bytes() (out []byte, err error) {
+	packetString, err := packet.String()
+	out = []byte(packetString)
+	return
+}
+
+func NewRequestActionPacket(actionId uint8, parameters map[string]interface{}) RequestActionPacket {
+	return RequestActionPacket{uuid.New(), actionId, parameters}
+}
+
+func NewResponseActionPacket(requestUUID uuid.UUID, approved bool, content map[string]interface{}) ResponseActionPacket {
+	return ResponseActionPacket{requestUUID, approved, content}
+}
+
+func ParsePacket(buffer *bytes.Buffer) (packet Packet, err error) {
+	identifier, err := readUntil(buffer, []byte(headerSeparator))
+	if err != nil {
+		return
+	}
+	var headers map[string]string
+	switch strings.TrimRight(string(identifier), "\r\n") {
+	case requestActionIdentifier:
+		//TODO: Request action packet
+	case responseActionIdentifier:
+		//TODO: Response action packet
+	case discoveryIdentifier:
+		headers, err = readHeaders(buffer)
+		if err != nil {
+			return
+		}
+		packetType, ok := headers["TYPE"]
+		if !ok {
+			return packet, errors.New("malformed packet: \"TYPE\" header not found")
+		}
+		packetHost, ok := headers["HOST"]
+		if !ok {
+			return packet, errors.New("malformed packet: \"HOST\" header not found")
+		}
+		var host, portString string
+		host, portString, err = net.SplitHostPort(packetHost)
+		if err != nil {
+			return packet, errors.New("malformed packet: invalid ip/port pair")
+		}
+		ip := net.ParseIP(host)
+		if ip == nil {
+			return packet, errors.New("malformed packet: hostname is not supported")
+		}
+		var port uint64
+		port, err = strconv.ParseUint(portString, 10, 16)
+		if err != nil {
+			return
+		}
+		switch packetType {
+		case requestDiscoveryType:
+			packetObj := NewRequestDiscoveryPacket(ip, uint16(port))
+			packet = &packetObj
+		case responseDiscoveryType:
+			packetObj := NewResponseDiscoveryPacket(ip, uint16(port))
+			packet = &packetObj
+		default:
+			return packet, errors.New(fmt.Sprintf("malformed packet: type \"%s\" not supported", packetType))
+		}
+	default:
+		err = errors.New("malformed packet: identifier not knwon")
+	}
+	return packet, err
+}
+
+func readUntil(buffer *bytes.Buffer, delim []byte) ([]byte, error) {
+	var err error
+	var readByte byte
+	out := make([]byte, 0, buffer.Cap())
+	read := make([]byte, len(delim))
+	for !bytes.Equal(read, delim) && err != io.EOF {
+		readByte, err = buffer.ReadByte()
+		if err != io.EOF {
+			out = append(out, readByte)
+		}
+		read[0] = read[1]
+		read[1] = readByte
+	}
+	readOut := make([]byte, len(out))
+	copy(readOut, out)
+	return readOut, err
+}
+
+func readHeaders(buffer *bytes.Buffer) (headers map[string]string, err error) {
+	headers = make(map[string]string)
+	headerRaw, err := readUntil(buffer, []byte(headerSeparator))
+	var headerPair []string
+	var header string
+	for err == nil && string(headerRaw) != "\r\n" {
+		header = strings.TrimRight(string(headerRaw), "\r\n")
+		headerPair, err = parseHeader(header)
+		if err != nil {
+			return
+		}
+		headers[headerPair[0]] = headerPair[1]
+		headerRaw, err = readUntil(buffer, []byte(headerSeparator))
+	}
+	return headers, err
+}
+
+func parseHeader(raw string) ([]string, error) {
+	headerValue := strings.SplitN(raw, ": ", 2)
+	if cap(headerValue) == 1 {
+		return []string{}, errors.New("invalid header line")
+	}
+	return headerValue, nil
 }
--- network/packet_test.go
+++ network/packet_test.go
@@ -2,6 +2,8 @@ package network
 
 import (
 	"bytes"
+	"io"
+	"log"
 	"net"
 	"testing"
 )
@@ -10,21 +12,27 @@ func TestRequestDiscoveryPacket(t *testing.T) {
 	var address = net.IPv4(127, 0, 0, 1)
 	var port uint16 = 8400
 	packet := NewRequestDiscoveryPacket(address, port)
-	packet_str := packet.String()
-	expected_str := "DYLLABLE-DISCOVERY\r\n" +
+	packetString, err := packet.String()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	expectedString := "DYLLABLE-DISCOVERY\r\n" +
 		"TYPE: DISCOVERY\r\n" +
 		"HOST: 127.0.0.1:8400\r\n" +
 		"\r\n"
-	if packet_str != expected_str {
-		t.Fatalf("Request DiscoveryPacket String() does not "+
-			"match to the expected.\nCurrent:\n%#v.\nExpected:\n%#v\n", packet_str, expected_str)
+	if packetString != expectedString {
+		t.Fatalf("RequestDiscoveryPacket String() does not "+
+			"match to the expected.\ncurrent:\n%#v.\nexpected:\n%#v\n", packetString, expectedString)
 	}
 
-	packet_bytes := packet.Bytes()
-	expected_bytes := []byte(expected_str)
-	if bytes.Compare(packet_bytes, expected_bytes) != 0 {
-		t.Fatalf("Request DiscoveryPacket Bytes() does not "+
-			"match to the expected.\nCurrent:\n%#v.\nExpected:\n%#v\n", packet_bytes, expected_bytes)
+	packetBytes, err := packet.Bytes()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	expectedBytes := []byte(expectedString)
+	if !bytes.Equal(packetBytes, expectedBytes) {
+		t.Fatalf("RequestDiscoveryPacket Bytes() does not "+
+			"match to the expected.\ncurrent:\n%#v.\nexpected:\n%#v\n", packetBytes, expectedBytes)
 	}
 }
 
@@ -32,20 +40,172 @@ func TestResponseDiscoveryPacket(t *testing.T) {
 	var address = net.IPv4(127, 0, 0, 1)
 	var port uint16 = 8400
 	packet := NewResponseDiscoveryPacket(address, port)
-	packet_str := packet.String()
-	expected_str := "DYLLABLE-DISCOVERY\r\n" +
+	packetString, err := packet.String()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	expectedString := "DYLLABLE-DISCOVERY\r\n" +
 		"TYPE: RUNNING-APP\r\n" +
 		"HOST: 127.0.0.1:8400\r\n" +
 		"\r\n"
-	if packet_str != expected_str {
-		t.Fatalf("Request DiscoveryPacket String() does not "+
-			"match to the expected.\nCurrent:\n%#v.\nExpected:\n%#v\n", packet_str, expected_str)
+	if packetString != expectedString {
+		t.Fatalf("ResponseDiscoveryPacket String() does not "+
+			"match to the expected.\ncurrent:\n%#v.\nexpected:\n%#v\n", packetString, expectedString)
 	}
 
-	packet_bytes := packet.Bytes()
-	expected_bytes := []byte(expected_str)
-	if bytes.Compare(packet_bytes, expected_bytes) != 0 {
-		t.Fatalf("Request DiscoveryPacket Bytes() does not "+
-			"match to the expected.\nCurrent:\n%#v.\nExpected:\n%#v\n", packet_bytes, expected_bytes)
+	packetBytes, err := packet.Bytes()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	expectedBytes := []byte(expectedString)
+	if !bytes.Equal(packetBytes, expectedBytes) {
+		t.Fatalf("ResponseDiscoveryPacket Bytes() does not "+
+			"match to the expected.\ncurrent:\n%#v.\nexpected:\n%#v\n", packetBytes, expectedBytes)
+	}
+}
+
+func TestParseRequestDiscoveryPacket(t *testing.T) {
+	var address = net.IPv4(127, 0, 0, 1)
+	var port uint16 = 8400
+	packetString := "DYLLABLE-DISCOVERY\r\n" +
+		"TYPE: DISCOVERY\r\n" +
+		"HOST: 127.0.0.1:8400\r\n" +
+		"\r\n"
+	packetBuffer := bytes.NewBuffer([]byte(packetString))
+	packet, err := ParsePacket(packetBuffer)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	discoveryPacket, ok := packet.(*DiscoveryPacket)
+	if !ok {
+		log.Fatalf("expected DiscoveryPacket object instead of %T", packet)
+	}
+	if !discoveryPacket.Address.Equal(address) {
+		log.Fatalf("expected parsed packet IP equals to \"127.0.0.1\" instead of \"%v\"", discoveryPacket.Address)
+	}
+	if discoveryPacket.Port != port {
+		log.Fatalf("expected parsed packet port equals to \"8400\" instead of \"%v\"", discoveryPacket.Port)
+	}
+	if discoveryPacket.Type != requestDiscoveryType {
+		log.Fatalf("expected parsed packet type equals to \"%s\" instead of \"%s\"", requestDiscoveryType, discoveryPacket.Type)
+	}
+}
+
+func TestParseResponseDiscoveryPacket(t *testing.T) {
+	var address = net.IPv4(127, 0, 0, 1)
+	var port uint16 = 8400
+	packetString := "DYLLABLE-DISCOVERY\r\n" +
+		"TYPE: RUNNING-APP\r\n" +
+		"HOST: 127.0.0.1:8400\r\n" +
+		"\r\n"
+	packetBuffer := bytes.NewBuffer([]byte(packetString))
+	packet, err := ParsePacket(packetBuffer)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	discoveryPacket, ok := packet.(*DiscoveryPacket)
+	if !ok {
+		log.Fatalf("expected DiscoveryPacket object instead of %T", packet)
+	}
+	if !discoveryPacket.Address.Equal(address) {
+		log.Fatalf("expected parsed packet IP equals to \"127.0.0.1\" instead of \"%v\"", discoveryPacket.Address)
+	}
+	if discoveryPacket.Port != port {
+		log.Fatalf("expected parsed packet port equals to \"8400\" instead of \"%v\"", discoveryPacket.Port)
+	}
+	if discoveryPacket.Type != responseDiscoveryType {
+		log.Fatalf("expected parsed packet type equals to \"%s\" instead of \"%s\"", responseDiscoveryType, discoveryPacket.Type)
+	}
+}
+
+func TestParseInvalidDiscoveryPacket(t *testing.T) {
+	invalidPackets := []string{
+		"TYPE: DISCOVERY\r\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DYSCOVERY\r\n" +
+			"TYPE: DISCOVERY\r\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\r\n" +
+			"TYPE: INVALID\r\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\r\n" +
+			"TYPE: RUNNING-APP\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\r\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\n" +
+			"TYPE: INVALID\r\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\r\n" +
+			"TYPE: INVALID\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\r\n",
+		"DYLLABLE-DISCOVERY\r\n" +
+			"TYPE: INVALID\n" +
+			"HOST: 127.0.0.1:8400\r\n" +
+			"\n",
+	}
+
+	var packetBuffer *bytes.Buffer
+	for _, packetString := range invalidPackets {
+		packetBuffer = bytes.NewBuffer([]byte(packetString))
+		_, err := ParsePacket(packetBuffer)
+		if err == nil {
+			log.Fatalf("following packet should be invalid: \n%s", packetString)
+		}
+	}
+}
+
+func TestReadUntil(t *testing.T) {
+	var bufferValue []byte
+	var buffer *bytes.Buffer
+	var delim = []byte("\r\n")
+	var read []byte
+	var err error
+
+	//Test if the function returns EOF when it does not find the delimeter
+	bufferValue = []byte("DYLLABLE-DISCOVERY")
+	buffer = bytes.NewBuffer(bufferValue)
+	read, err = readUntil(buffer, delim)
+	if err != io.EOF {
+		log.Fatalf("1: when the delimiter is not in the buffer, the function should return io.EOF")
+	}
+	if !bytes.Equal(read, bufferValue) {
+		log.Fatalf("2: when the delimiter is not in the buffer, the function should return the same value in the buffer.  %v != %v", read, bufferValue)
+	}
+
+	//Test if the function allows call multiple times in the same buffer (already readed buffer)
+	bufferValue = []byte("DYLLABLE-DISCOVERY\r\nTYPE: DISCOVERY\r\n\r\n")
+
+	buffer = bytes.NewBuffer(bufferValue)
+	read, err = readUntil(buffer, delim)
+	if err != nil {
+		log.Fatalf("3: %v", err)
+	}
+	if !bytes.Equal(read, []byte("DYLLABLE-DISCOVERY\r\n")) {
+		log.Fatalf("4: %#v != %#v", string(read), "DYLLABLE-DISCOVERY\r\n")
+	}
+	read, err = readUntil(buffer, delim)
+	if err != nil {
+		log.Fatalf("4: %v", err)
+	}
+	if !bytes.Equal(read, []byte("TYPE: DISCOVERY\r\n")) {
+		log.Fatalf("5: %#v != %#v", string(read), "TYPE: DISCOVERY\r\n")
+	}
+	read, err = readUntil(buffer, delim)
+	if err != nil {
+		log.Fatalf("6: %v", err)
+	}
+	if !bytes.Equal(read, delim) {
+		log.Fatalf("7: %#v != %#v", string(read), string(delim))
+	}
+	read, err = readUntil(buffer, delim)
+	if err != io.EOF {
+		log.Fatalf("8: the function should return io.EOF when buffer has reached to the end")
 	}
 }
